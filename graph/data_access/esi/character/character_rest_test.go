da9e91d72300c43f452a5cc927d922c51a000013
Bryan Crow <cryan.brow@gmail.com>
2022-02-02 22:58:21 -0600

    Adding Character Tests

---
+++ graph/data_access/esi/character/character_rest_test.go
@@ -0,0 +1,135 @@
+package character
+
+import (
+	"bytes"
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/cryanbrow/eve-graphql-go/graph/configuration"
+)
+
+func TestSuccessful_CharacterByID(t *testing.T) {
+	jsonResponse := `{
+		"birthday": "2008-04-01T10:24:00Z",
+		"bloodline_id": 3,
+		"corporation_id": 1000182,
+		"description": "",
+		"gender": "male",
+		"name": "Gar Orga",
+		"race_id": 2,
+		"security_status": 0
+	  }`
+
+	b := []byte(jsonResponse)
+
+	mock_rest_helper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(base_url string, verb string, body bytes.Buffer, additional_query_params []configuration.Key_value, redis_query_key string) ([]byte, http.Header, error) {
+			return b, nil, nil
+		},
+	}
+	rest_helper = mock_rest_helper
+
+	var test_id int = 1
+
+	resp, err := CharacterByID(&test_id)
+	if err != nil {
+		t.Errorf("Error was not nil, %v", err)
+	}
+	var resp_name string = "Gar Orga"
+	if *resp.Name != resp_name {
+		t.Errorf("Response was not as expected")
+	}
+
+}
+
+func TestFailNilID_CharacterByID(t *testing.T) {
+	jsonResponse := `{
+		"birthday": "2008-04-01T10:24:00Z",
+		"bloodline_id": 3,
+		"corporation_id": 1000182,
+		"description": "",
+		"gender": "male",
+		"name": "Gar Orga",
+		"race_id": 2,
+		"security_status": 0
+	  }`
+
+	b := []byte(jsonResponse)
+
+	mock_rest_helper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(base_url string, verb string, body bytes.Buffer, additional_query_params []configuration.Key_value, redis_query_key string) ([]byte, http.Header, error) {
+			return b, nil, nil
+		},
+	}
+	rest_helper = mock_rest_helper
+
+	var test_id *int = nil
+
+	_, err := CharacterByID(test_id)
+	if err == nil {
+		t.Error("Error is nil")
+	} else if err.Error() != "nil id" {
+		t.Errorf("Wrong error text: %s", err.Error())
+	}
+
+}
+
+func TestFailRestCall_CharacterByID(t *testing.T) {
+	mock_rest_helper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(base_url string, verb string, body bytes.Buffer, additional_query_params []configuration.Key_value, redis_query_key string) ([]byte, http.Header, error) {
+			return nil, nil, errors.New("failure")
+		},
+	}
+	rest_helper = mock_rest_helper
+
+	var test_id int = 1
+
+	_, err := CharacterByID(&test_id)
+	if err == nil {
+		t.Error("Error is nil")
+	} else if err.Error() != "failure" {
+		t.Errorf("Wrong error text: %s", err.Error())
+	}
+
+}
+
+func TestFailUnmarshal_CharacterByID(t *testing.T) {
+	jsonResponse := `{{
+		"birthday": "2008-04-01T10:24:00Z",
+		"bloodline_id": 3,
+		"corporation_id": 1000182,
+		"description": "",
+		"gender": "male",
+		"name": "Gar Orga",
+		"race_id": 2,
+		"security_status": 0
+	  }`
+
+	b := []byte(jsonResponse)
+
+	mock_rest_helper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(base_url string, verb string, body bytes.Buffer, additional_query_params []configuration.Key_value, redis_query_key string) ([]byte, http.Header, error) {
+			return b, nil, nil
+		},
+	}
+	rest_helper = mock_rest_helper
+
+	var test_id int = 1
+
+	_, err := CharacterByID(&test_id)
+	if err == nil {
+		t.Error("Error is nil")
+	}
+
+}
+
+type MockMakeCachingRESTCallType func(base_url string, verb string, body bytes.Buffer, additional_query_params []configuration.Key_value, redis_query_key string) ([]byte, http.Header, error)
+
+type MockRestHelper struct {
+	MockMakeCachingRESTCall MockMakeCachingRESTCallType
+}
+
+func (m *MockRestHelper) MakeCachingRESTCall(base_url string, verb string, body bytes.Buffer, additional_query_params []configuration.Key_value, redis_query_key string) ([]byte, http.Header, error) {
+	return m.MockMakeCachingRESTCall(base_url, verb, body, additional_query_params, redis_query_key)
+}
