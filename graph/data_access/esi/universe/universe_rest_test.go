055d892cf50288441c7d20c517c53ef0c82334c0
Bryan Crow <cryan.brow@gmail.com>
2022-02-05 08:34:54 -0600

    Graphic and Group Tests

--- graph/data_access/esi/universe/universe_rest_test.go
+++ graph/data_access/esi/universe/universe_rest_test.go
@@ -1206,6 +1206,356 @@ func TestFailUnmarshal_GraphicByID(t *testing.T) {
 }
 
 /***************************************
+*              GroupByID               *
+***************************************/
+
+func TestSuccessful_GroupByID(t *testing.T) {
+	jsonResponse := `{
+		"category_id": 6,
+		"group_id": 25,
+		"name": "Frigate",
+		"published": true,
+		"types": [
+		  582,
+		  583,
+		  584,
+		  585,
+		  586,
+		  587,
+		  589,
+		  590,
+		  591,
+		  592,
+		  593,
+		  594,
+		  595,
+		  597,
+		  598,
+		  599,
+		  600,
+		  602,
+		  603,
+		  605,
+		  607,
+		  608,
+		  609,
+		  613,
+		  614,
+		  616,
+		  618,
+		  619,
+		  1896,
+		  1898,
+		  1900,
+		  1902,
+		  2161,
+		  3532,
+		  3751,
+		  3753,
+		  3766,
+		  3768,
+		  11019,
+		  11940,
+		  11942,
+		  17360,
+		  17619,
+		  17703,
+		  17705,
+		  17707,
+		  17812,
+		  17841,
+		  17924,
+		  17926,
+		  17928,
+		  17930,
+		  17932,
+		  29248,
+		  32880,
+		  32983,
+		  32985,
+		  32987,
+		  32989,
+		  33190,
+		  33468,
+		  33655,
+		  33657,
+		  33659,
+		  33661,
+		  33663,
+		  33665,
+		  33667,
+		  33669,
+		  33677,
+		  33816,
+		  34443,
+		  37453,
+		  37454,
+		  37455,
+		  37456,
+		  47269,
+		  54731,
+		  58745
+		]
+	  }`
+
+	b := []byte(jsonResponse)
+
+	mock_rest_helper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(base_url string, verb string, body bytes.Buffer, additional_query_params []configuration.Key_value, redis_query_key string) ([]byte, http.Header, error) {
+			return b, nil, nil
+		},
+	}
+	rest_helper = mock_rest_helper
+
+	var test_id int = 25
+
+	resp, err := GroupByID(&test_id)
+	if err != nil {
+		t.Errorf("Error was not nil, %v", err)
+	}
+	var resp_name string = "Frigate"
+	if *resp.Name != resp_name {
+		t.Errorf("Response was not as expected")
+	}
+
+}
+
+func TestFailNilID_GroupByID(t *testing.T) {
+	jsonResponse := `{
+		"category_id": 6,
+		"group_id": 25,
+		"name": "Frigate",
+		"published": true,
+		"types": [
+		  582,
+		  583,
+		  584,
+		  585,
+		  586,
+		  587,
+		  589,
+		  590,
+		  591,
+		  592,
+		  593,
+		  594,
+		  595,
+		  597,
+		  598,
+		  599,
+		  600,
+		  602,
+		  603,
+		  605,
+		  607,
+		  608,
+		  609,
+		  613,
+		  614,
+		  616,
+		  618,
+		  619,
+		  1896,
+		  1898,
+		  1900,
+		  1902,
+		  2161,
+		  3532,
+		  3751,
+		  3753,
+		  3766,
+		  3768,
+		  11019,
+		  11940,
+		  11942,
+		  17360,
+		  17619,
+		  17703,
+		  17705,
+		  17707,
+		  17812,
+		  17841,
+		  17924,
+		  17926,
+		  17928,
+		  17930,
+		  17932,
+		  29248,
+		  32880,
+		  32983,
+		  32985,
+		  32987,
+		  32989,
+		  33190,
+		  33468,
+		  33655,
+		  33657,
+		  33659,
+		  33661,
+		  33663,
+		  33665,
+		  33667,
+		  33669,
+		  33677,
+		  33816,
+		  34443,
+		  37453,
+		  37454,
+		  37455,
+		  37456,
+		  47269,
+		  54731,
+		  58745
+		]
+	  }`
+
+	b := []byte(jsonResponse)
+
+	mock_rest_helper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(base_url string, verb string, body bytes.Buffer, additional_query_params []configuration.Key_value, redis_query_key string) ([]byte, http.Header, error) {
+			return b, nil, nil
+		},
+	}
+	rest_helper = mock_rest_helper
+
+	var test_id *int = nil
+
+	_, err := GroupByID(test_id)
+	if err == nil {
+		t.Error("Error is nil")
+	} else if err.Error() != "nil id" {
+		t.Errorf("Wrong error text: %s", err.Error())
+	}
+
+}
+
+func TestFailRestCall_GroupByID(t *testing.T) {
+	mock_rest_helper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(base_url string, verb string, body bytes.Buffer, additional_query_params []configuration.Key_value, redis_query_key string) ([]byte, http.Header, error) {
+			return nil, nil, errors.New("failure")
+		},
+	}
+	rest_helper = mock_rest_helper
+
+	var test_id int = 25
+
+	_, err := GroupByID(&test_id)
+	if err == nil {
+		t.Error("Error is nil")
+	} else if err.Error() != "failure" {
+		t.Errorf("Wrong error text: %s", err.Error())
+	}
+
+}
+
+func TestFailUnmarshal_GroupByID(t *testing.T) {
+	jsonResponse := `{{
+		"category_id": 6,
+		"group_id": 25,
+		"name": "Frigate",
+		"published": true,
+		"types": [
+		  582,
+		  583,
+		  584,
+		  585,
+		  586,
+		  587,
+		  589,
+		  590,
+		  591,
+		  592,
+		  593,
+		  594,
+		  595,
+		  597,
+		  598,
+		  599,
+		  600,
+		  602,
+		  603,
+		  605,
+		  607,
+		  608,
+		  609,
+		  613,
+		  614,
+		  616,
+		  618,
+		  619,
+		  1896,
+		  1898,
+		  1900,
+		  1902,
+		  2161,
+		  3532,
+		  3751,
+		  3753,
+		  3766,
+		  3768,
+		  11019,
+		  11940,
+		  11942,
+		  17360,
+		  17619,
+		  17703,
+		  17705,
+		  17707,
+		  17812,
+		  17841,
+		  17924,
+		  17926,
+		  17928,
+		  17930,
+		  17932,
+		  29248,
+		  32880,
+		  32983,
+		  32985,
+		  32987,
+		  32989,
+		  33190,
+		  33468,
+		  33655,
+		  33657,
+		  33659,
+		  33661,
+		  33663,
+		  33665,
+		  33667,
+		  33669,
+		  33677,
+		  33816,
+		  34443,
+		  37453,
+		  37454,
+		  37455,
+		  37456,
+		  47269,
+		  54731,
+		  58745
+		]
+	  }`
+
+	b := []byte(jsonResponse)
+
+	mock_rest_helper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(base_url string, verb string, body bytes.Buffer, additional_query_params []configuration.Key_value, redis_query_key string) ([]byte, http.Header, error) {
+			return b, nil, nil
+		},
+	}
+	rest_helper = mock_rest_helper
+
+	var test_id int = 25
+
+	_, err := GroupByID(&test_id)
+	if err == nil {
+		t.Error("Error is nil")
+	}
+
+}
+
+/***************************************
 *             MOCK SECTION             *
 ***************************************/
 
