c3031fe78c4696a86c520e6ce93b74090bcbc3c4
Bryan Crow <cryan.brow@gmail.com>
2022-02-04 19:24:05 -0600

    Category Tests

--- graph/data_access/esi/universe/universe_rest_test.go
+++ graph/data_access/esi/universe/universe_rest_test.go
@@ -542,6 +542,143 @@ func TestFailRestNotInCache_BloodlineByID(t *testing.T) {
 }
 
 /***************************************
+*           CategoryBeltByID           *
+***************************************/
+
+func TestSuccessful_CategoryByID(t *testing.T) {
+	jsonResponse := `{
+		"category_id": 5,
+		"groups": [
+		  23,
+		  24,
+		  872,
+		  876,
+		  943,
+		  1301,
+		  1311,
+		  1739,
+		  1875
+		],
+		"name": "Accessories",
+		"published": true
+	  }`
+
+	b := []byte(jsonResponse)
+
+	mock_rest_helper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(base_url string, verb string, body bytes.Buffer, additional_query_params []configuration.Key_value, redis_query_key string) ([]byte, http.Header, error) {
+			return b, nil, nil
+		},
+	}
+	rest_helper = mock_rest_helper
+
+	var test_id int = 5
+
+	resp, err := CategoryByID(&test_id)
+	if err != nil {
+		t.Errorf("Error was not nil, %v", err)
+	}
+	var resp_name string = "Accessories"
+	if *resp.Name != resp_name {
+		t.Errorf("Response was not as expected")
+	}
+
+}
+
+func TestFailNilID_CategoryByID(t *testing.T) {
+	jsonResponse := `{
+		"category_id": 5,
+		"groups": [
+		  23,
+		  24,
+		  872,
+		  876,
+		  943,
+		  1301,
+		  1311,
+		  1739,
+		  1875
+		],
+		"name": "Accessories",
+		"published": true
+	  }`
+
+	b := []byte(jsonResponse)
+
+	mock_rest_helper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(base_url string, verb string, body bytes.Buffer, additional_query_params []configuration.Key_value, redis_query_key string) ([]byte, http.Header, error) {
+			return b, nil, nil
+		},
+	}
+	rest_helper = mock_rest_helper
+
+	var test_id *int = nil
+
+	_, err := CategoryByID(test_id)
+	if err == nil {
+		t.Error("Error is nil")
+	} else if err.Error() != "nil id" {
+		t.Errorf("Wrong error text: %s", err.Error())
+	}
+
+}
+
+func TestFailRestCall_CategoryByID(t *testing.T) {
+	mock_rest_helper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(base_url string, verb string, body bytes.Buffer, additional_query_params []configuration.Key_value, redis_query_key string) ([]byte, http.Header, error) {
+			return nil, nil, errors.New("failure")
+		},
+	}
+	rest_helper = mock_rest_helper
+
+	var test_id int = 1
+
+	_, err := CategoryByID(&test_id)
+	if err == nil {
+		t.Error("Error is nil")
+	} else if err.Error() != "failure" {
+		t.Errorf("Wrong error text: %s", err.Error())
+	}
+
+}
+
+func TestFailUnmarshal_CategoryByID(t *testing.T) {
+	jsonResponse := `{{
+		"category_id": 5,
+		"groups": [
+		  23,
+		  24,
+		  872,
+		  876,
+		  943,
+		  1301,
+		  1311,
+		  1739,
+		  1875
+		],
+		"name": "Accessories",
+		"published": true
+	  }`
+
+	b := []byte(jsonResponse)
+
+	mock_rest_helper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(base_url string, verb string, body bytes.Buffer, additional_query_params []configuration.Key_value, redis_query_key string) ([]byte, http.Header, error) {
+			return b, nil, nil
+		},
+	}
+	rest_helper = mock_rest_helper
+
+	var test_id int = 5
+
+	_, err := CategoryByID(&test_id)
+	if err == nil {
+		t.Error("Error is nil")
+	}
+
+}
+
+/***************************************
 *             MOCK SECTION             *
 ***************************************/
 
