ca9bb4fea6e262dbefd74f3d23c15977e98ad014
Bryan Crow <cryan.brow@gmail.com>
2022-02-07 19:22:37 -0600

    order for region tests

--- graph/data_access/esi/market/market_rest.go
+++ graph/data_access/esi/market/market_rest.go
@@ -2,93 +2,11 @@ package market
 
 import (
 	"bytes"
-	"encoding/json"
-	"errors"
-	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/cryanbrow/eve-graphql-go/graph/configuration"
-	"github.com/cryanbrow/eve-graphql-go/graph/data_access/esi/universe"
-	model "github.com/cryanbrow/eve-graphql-go/graph/generated/model"
 	"github.com/cryanbrow/eve-graphql-go/graph/helpers"
-	local_model "github.com/cryanbrow/eve-graphql-go/graph/model"
-	log "github.com/sirupsen/logrus"
 )
-
-func OrdersForRegion(regionID *int, orderType *model.Ordertype, typeID *int, page *int) (*model.OrderWrapper, error) {
-	log.WithFields(log.Fields{"regionID": regionID, "typeID": typeID, "orderType": orderType}).Info("OrdersForRegion Called")
-	orderList := make([]*model.Order, 0)
-	baseUrl := fmt.Sprintf("%s/markets/%s/orders/", configuration.AppConfig.Esi.Default.Url, strconv.Itoa(*regionID))
-
-	redisKey := "OrdersForRegion:" + strconv.Itoa(*regionID) + ":" + orderType.String()
-
-	queryParams := make([]configuration.Key_value, 2)
-	kv := new(configuration.Key_value)
-	kv.Key = "page"
-	kv.Value = strconv.Itoa(*page)
-	queryParams = append(queryParams, *kv)
-
-	if typeID != nil {
-		redisKey = redisKey + ":" + strconv.Itoa(*typeID)
-		kv.Key = "type_id"
-		kv.Value = strconv.Itoa(*typeID)
-		queryParams = append(queryParams, *kv)
-	}
-
-	redisKey = redisKey + ":" + strconv.Itoa(*page)
-
-	orderResult, pages, err := ordersForRegionREST(baseUrl, queryParams, redisKey)
-
-	if err == nil {
-		orderList = append(orderList, orderResult...)
-	} else {
-		log.WithFields(log.Fields{"regionID": regionID, "typeID": typeID, "orderType": orderType}).Errorf("First page query for Orders has error : %v", err)
-		println(err)
-	}
-
-	returnOrders := new(model.OrderWrapper)
-	returnOrders.List = orderList
-	returnOrders.Xpages = &pages
-
-	return returnOrders, nil
-}
-
-func OrdersForRegionByName(region *string, orderType *model.Ordertype, typeName *string, page *int) (*model.OrderWrapper, error) {
-	regionID, err := universe.IdForName(region, local_model.REGIONS)
-	if err != nil {
-		return nil, errors.New("unknown name for region")
-	}
-	typeID, err := universe.IdForName(typeName, local_model.INVENTORY_TYPES)
-	if err != nil {
-		return nil, errors.New("unknown name for typeName")
-	}
-	orders, err := OrdersForRegion(&regionID, orderType, &typeID, page)
-	if err != nil {
-		return nil, err
-	}
-
-	return orders, nil
-}
-
-func ordersForRegionREST(url string, additionalQueryParams []configuration.Key_value, redisKey string) ([]*model.Order, int, error) {
-	var orders []*model.Order
-	var pages = 0
-	var buffer bytes.Buffer
-	responseBytes, header, err := restHelper.MakeCachingRESTCall(url, http.MethodGet, buffer, additionalQueryParams, redisKey)
-	if err != nil {
-		return orders, 0, err
-	}
-
-	pages, _ = strconv.Atoi(header.Get("x-pages"))
-
-	if err := json.Unmarshal(responseBytes, &orders); err != nil {
-		log.WithFields(log.Fields{"url": url}).Errorf("Could not unmarshal reponseBytes. : %v", err.Error())
-		return orders, 0, err
-	}
-
-	return orders, pages, nil
-}
 
 type RestHelper interface {
 	MakeCachingRESTCall(baseUrl string, verb string, body bytes.Buffer, additionalQueryParams []configuration.Key_value, redisQueryKey string) ([]byte, http.Header, error)
---
+++ graph/data_access/esi/market/orders_for_region_test.go
@@ -0,0 +1,168 @@
+package market
+
+import (
+	"bytes"
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/cryanbrow/eve-graphql-go/graph/configuration"
+	"github.com/cryanbrow/eve-graphql-go/graph/generated/model"
+	"github.com/cryanbrow/eve-graphql-go/graph/helpers"
+)
+
+/***************************************
+*             OrdersForRegion              *
+***************************************/
+
+func TestSuccessfulOrdersForRegion(t *testing.T) {
+	jsonResponse := `[
+		{
+		  "duration": 90,
+		  "is_buy_order": true,
+		  "issued": "2022-01-30T18:47:21Z",
+		  "location_id": 1035026346682,
+		  "min_volume": 1,
+		  "order_id": 6161383608,
+		  "price": 2280000,
+		  "range": "40",
+		  "system_id": 30000732,
+		  "type_id": 44992,
+		  "volume_remain": 155,
+		  "volume_total": 500
+		}]`
+
+	b := []byte(jsonResponse)
+
+	header := http.Header{
+		"x-pages": []string{"1"},
+	}
+
+	mockRestHelper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(baseUrl string, verb string, body bytes.Buffer, additionalQueryParams []configuration.Key_value, redisQueryKey string) ([]byte, http.Header, error) {
+			return b, header, nil
+		},
+	}
+	restHelper = mockRestHelper
+
+	var regionId int = 10000008
+	var typeId int = 44992
+	var page int = 1
+	var orderType model.Ordertype = model.OrdertypeAll
+
+	resp, err := OrdersForRegion(&regionId, &orderType, &typeId, &page)
+	if err != nil {
+		t.Errorf(helpers.ErrorWasNotNil, err)
+	}
+	var responseName int = 30000732
+	if *resp.List[0].SystemID != responseName {
+		t.Errorf(helpers.ResponseWasNotAsExpected)
+	}
+
+}
+
+func TestFailNilTypeIDOrdersForRegion(t *testing.T) {
+	jsonResponse := `[
+		{
+		  "duration": 90,
+		  "is_buy_order": true,
+		  "issued": "2022-01-30T18:47:21Z",
+		  "location_id": 1035026346682,
+		  "min_volume": 1,
+		  "order_id": 6161383608,
+		  "price": 2280000,
+		  "range": "40",
+		  "system_id": 30000732,
+		  "type_id": 44992,
+		  "volume_remain": 155,
+		  "volume_total": 500
+		}]`
+
+	b := []byte(jsonResponse)
+
+	header := http.Header{
+		"x-pages": []string{"5"},
+	}
+
+	mockRestHelper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(baseUrl string, verb string, body bytes.Buffer, additionalQueryParams []configuration.Key_value, redisQueryKey string) ([]byte, http.Header, error) {
+			return b, header, nil
+		},
+	}
+	restHelper = mockRestHelper
+
+	var regionId int = 10000008
+	var typeId *int = nil
+	var page int = 1
+	var orderType model.Ordertype = model.OrdertypeAll
+
+	resp, err := OrdersForRegion(&regionId, &orderType, typeId, &page)
+	if err != nil {
+		t.Errorf(helpers.ErrorWasNotNil, err)
+	}
+	var responseName int = 30000732
+	if *resp.List[0].SystemID != responseName {
+		t.Errorf(helpers.ResponseWasNotAsExpected)
+	}
+
+}
+
+func TestFailRestCallOrdersForRegion(t *testing.T) {
+	mockRestHelper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(baseUrl string, verb string, body bytes.Buffer, additionalQueryParams []configuration.Key_value, redisQueryKey string) ([]byte, http.Header, error) {
+			return nil, nil, errors.New("failure")
+		},
+	}
+	restHelper = mockRestHelper
+
+	var regionId int = 10000008
+	var typeId int = 44992
+	var page int = 1
+	var orderType model.Ordertype = model.OrdertypeAll
+
+	_, err := OrdersForRegion(&regionId, &orderType, &typeId, &page)
+	if err == nil {
+		t.Error(helpers.NilError)
+	} else if err.Error() != "failure" {
+		t.Errorf(helpers.WrongErrorText, err.Error())
+	}
+
+}
+
+func TestFailUnmarshalOrdersForRegion(t *testing.T) {
+	jsonResponse := `[{
+		{
+		  "duration": 90,
+		  "is_buy_order": true,
+		  "issued": "2022-01-30T18:47:21Z",
+		  "location_id": 1035026346682,
+		  "min_volume": 1,
+		  "order_id": 6161383608,
+		  "price": 2280000,
+		  "range": "40",
+		  "system_id": 30000732,
+		  "type_id": 44992,
+		  "volume_remain": 155,
+		  "volume_total": 500
+		}]`
+
+	b := []byte(jsonResponse)
+
+	mockRestHelper := &MockRestHelper{
+		MockMakeCachingRESTCall: func(baseUrl string, verb string, body bytes.Buffer, additionalQueryParams []configuration.Key_value, redisQueryKey string) ([]byte, http.Header, error) {
+			return b, nil, nil
+		},
+	}
+	restHelper = mockRestHelper
+
+	var regionId int = 10000008
+	var typeId int = 44992
+	var page int = 1
+	var orderType model.Ordertype = model.OrdertypeAll
+
+	_, err := OrdersForRegion(&regionId, &orderType, &typeId, &page)
+	if err == nil {
+		t.Error(helpers.NilError)
+	}
+
+}
