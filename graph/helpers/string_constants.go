cbed3338cd938041d9920fdd72c0ff43e9939f24
Bryan Crow <cryan.brow@gmail.com>
2022-02-13 16:42:38 -0600

    Adding godoc comments.

--- graph/helpers/string_constants.go
+++ graph/helpers/string_constants.go
@@ -2,9 +2,21 @@ package helpers
 
 //NilError is a string constant for unit testing.
 const NilError = "error is nil"
+
+//NilError is a string constant for unit testing.
 const WrongErrorText = "Wrong error text: %s"
+
+//NilError is a string constant saying nil id.
 const NilID = "nil id"
+
+//NilError is a string constant saying could not unmarshal responseBytes.
 const CouldNotUnmarshalResponseBytes = "Could not unmarshal reponseBytes. %v"
+
+//NilError is a string constant saying Failure Marshalling.
 const FailureMarshaling = "Failure Marshalling: %v"
+
+//NilError is a string constant for unit testing.
 const ErrorWasNotNil = "Error was not nil, %v"
+
+//NilError is a string constant for unit testing.
 const ResponseWasNotAsExpected = "Response was not as expected"
